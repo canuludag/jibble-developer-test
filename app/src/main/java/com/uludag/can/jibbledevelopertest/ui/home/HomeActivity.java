e5ee1137c07fb9e9b0b560a181a7b9856b2d66ae
canuludag <cucanuludag@gmail.com>
2018-02-09 18:45:38 +0300

    first commit

---
+++ app/src/main/java/com/uludag/can/jibbledevelopertest/ui/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.uludag.can.jibbledevelopertest.ui.home;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.uludag.can.jibbledevelopertest.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
